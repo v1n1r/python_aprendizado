3393b4313409fb2a055364da3711099f93507c4b
V1n1r <vini.vini.rodriguesa@gmail.com>
2024-11-20 17:30:46 -0300

    Adiciona o arquivo variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,24 @@
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = 'Fábio', 47
+estado = True
+
+# Função Type()
+# print(type(media))
+# print(type(n2))
+# print(type(nome))
+# print(type(estado))
+# print(type(1+2j))
+
+
+# Função isinstance()
+# a = 10
+# b = 'sol'
+# print(isinstance(a, int))
+# print(isinstance(b, int))
+# print(isinstance(a, (int, float)))
+
+a = 40
+c = 3
+r = a * c
+print(r)
