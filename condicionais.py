e197df130867f3a00b964236cc55d5cf414095eb
V1n1r <vini.vini.rodriguesa@gmail.com>
2024-11-20 18:19:35 -0300

    Adicionar o arquivo condicionais.py

---
+++ condicionais.py
@@ -0,0 +1,21 @@
+#simples, composto, Encadeado
+
+n1 = n2 = 0
+media = 0.0
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+
+# Calcular a média aritmética das notas
+media = (n1 + n2) / 2
+
+if (media >= 7):
+    print('Resultado: Aprovado!')
+    print('Parabéns!')
+elif(media >=5 ):
+    print('Você está de recuperação')
+else:
+    print('Aluno reprovado...')
+    
+print('Sua média é {}'.format(media))
+    
