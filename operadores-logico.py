37ab6d521913c1f0a03f83375e9385fdfbfc2f9c
V1n1r <vini.vini.rodriguesa@gmail.com>
2024-11-20 18:06:38 -0300

    Adicionar o arquivo operadores-logico.py aula boson treinamentos

---
+++ operadores-logico.py
@@ -0,0 +1,20 @@
+# idade = 25
+# altura = 1.75
+
+# resultado = (idade >= 18) and (altura >= 1.70)
+# msg = 'Pode participar do evento?' + str(resultado)
+
+# print(msg)
+
+# Programa de disparo de alarme
+# porta = 'f'
+# janela = 'f'
+
+# alarme = (porta == 'a') or (janela == 'a')
+# msg = 'Alarme disparado?' + str(alarme)
+# print(msg)
+
+tem_dinheiro = False
+tem_dinheiro = not tem_dinheiro
+msg = 'Tem dinheiro? ' + str(tem_dinheiro)
+print(msg)
