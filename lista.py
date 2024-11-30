33c333aab480f3e2296a1b0b3d06929b5f632195
V1n1r <vini.vini.rodriguesa@gmail.com>
2024-11-30 17:28:08 -0300

    Listas

---
+++ lista.py
@@ -0,0 +1,48 @@
+# Lista: representa uma sequência de valores
+
+# Sintaxe: nome_lista = [valores]
+
+# notas = [5,7,8,6,9]
+# print(notas)
+
+# n1 = [4,6,7,8,0,3]
+# n2 = [1,6,3,0,12,4]
+# valores = n1 + n2
+# print(valores)
+# print(type(valores))
+
+# n1 = [4,6,7,8,0,3]
+# n2 = [1,6,3,0,12,4]
+# valores = n1 + n2
+# valores[0] = 9
+# print(valores[3:4])
+
+#n1 = [4,6,7,8,0,3]
+#n2 = [1,6,3,0,12,4]
+#valores = n1 + n2
+# print(len(valores))
+# print(sorted(valores, reverse=True))
+# print(sum(valores))
+# print(min(valores))
+# print(max(valores))
+
+# valores.append(13)
+# print(valores)
+
+# valores.pop()
+# print(valores)
+
+# valores.pop(3)
+# print(valores)
+
+# valores.insert(3, 21)
+# print(valores)
+
+# print(12 in valores)
+# print(17 in valores)
+
+planetas = ['Mercúrio', 'Vênus', 'Marte','Saturno', 'Urano', 'Netuno']
+for planeta in planetas:
+    print(planeta)
+
+    
