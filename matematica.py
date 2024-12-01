acc4677aee84e78bb3cab2b4d28e828ea9d1ace0
V1n1r <vini.vini.rodriguesa@gmail.com>
2024-12-01 20:45:57 -0300

    Funções matematicas built-in e o módul Math

---
+++ matematica.py
@@ -0,0 +1,27 @@
+# Funções builtin
+
+# valores = [2,5,8,-1,0,11,7,-6]
+# print(max(valores))
+# print(min(valores))
+# a = -5
+# b = 4
+# print(abs(a))
+# print(pow(a, b))
+# c = 2.34365476
+# print(round(c,1))
+
+import math
+
+x = 8
+y = 100
+
+# raiz_quadrada = math.sqrt(x)
+# print(math.floor(raiz_quadrada))
+# print(round(raiz_quadrada, 2))
+
+# logaritmo = math.log10(y)
+# print(logaritmo)
+
+# print(math.pi)
+print(math.factorial(x))
+print(x / math.inf)
