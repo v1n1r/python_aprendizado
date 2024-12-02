9e38ba7fc8433925e344595920e299b19dc1867b
V1n1r <vini.vini.rodriguesa@gmail.com>
2024-12-01 21:10:56 -0300

    Manipulação de strings

---
+++ strings.py
@@ -0,0 +1,72 @@
+# nome = 'Curso de Python'
+# instrutor = 'Fábio'
+# letra = nome[2]
+
+# print(letra)
+# print(len(nome))
+# print(nome + ' com ' + instrutor)
+
+# frase = 'Vamos aprender Python hoje.'
+# # palavras = frase.split()
+# # print(palavras[1])
+# # for letra in frase:
+# #     print(letra)
+
+# print(frase[-3:])
+
+# email = input('Digite seu enereço de email: ')
+# arroba = email.find('@')
+# print(arroba)
+# usuario = email[0:arroba]
+# dominio = email[arroba+1:]
+# print(usuario)
+# print(dominio)
+
+# produtos = 'carbono de sodio e oxido de zinco'
+# print('sodio' in produtos)
+# print('magnesio' not in produtos)
+
+# item = 'hipoclorito'
+# pos = item.find('clor')
+# print(pos)
+# pos = item.find('flu')
+# print(pos)
+
+# objeto_celeste = 'galaxia esPiral M31'
+# print(objeto_celeste.upper())
+# print(objeto_celeste.lower())
+# print(objeto_celeste.capitalize())
+# print(objeto_celeste.title())
+
+# suplemento = 'cloreto de magnesio'
+# n_suplemento = suplemento.replace('magnesio', 'zinco')
+# print(suplemento)
+# print(n_suplemento)
+
+# frase = '     Omega 3 e bom para a saude!    '
+# print(frase)
+# print(frase.lstrip())
+# print(frase.rstrip())
+# print(frase.strip())
+
+# fruta = 'Abacate'
+# print(fruta)
+# print(fruta.rjust(20))
+# print(fruta.center(20))
+# print(fruta.ljust(20))
+# print(fruta.center(20, "-"))
+
+# p = 'Bóson Treinamnetos'
+# print(p.startswith('Bó'))
+# print(p.endswith('o'))
+
+# Docstrings
+texto = """
+Docstring é uma espécie de documentação que podemos inserir dentro de um módulo, função
+ou classe no Python, entre outros locais.
+    Respeita deslocamento do texto e é multilinhas
+"""
+
+print(texto)
+
+
